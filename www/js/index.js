26de33156b9ea5c5921b486c07176d1578deae36
Thomas Wiegand <wieg0002@algonquinlive.com>
2014-12-09 18:37:51 -0500

    straight copy

---
+++ www/js/index.js
@@ -0,0 +1,95 @@
+/**
+ * IM RJ TW
+ */
+
+var google_map; //google.maps.Map
+var lat_in = 45.3469;
+var lng_in = -75.7598;
+var city_code = "oc";
+var mode = "dynamic";
+var zoom_level = 17;
+var wait_widow = "wait_window"
+
+var IS_QUIK = false;
+var IS_DEBUG = false;
+
+$(document).ready(function ()
+{
+    $.support.cors = true;
+
+    //alert("Set Breakpoints Now");
+
+    $(document).bind('deviceready', function ()
+    {
+
+        startUpPage.init("start_up_page", "map_canvas", "bus_list", "status_page");
+        startUpPage.checkConnection();
+        //Status page initialization
+        statusPage.init("status_page", "status_list", "refresh_btn", "map_btn");
+        busListPage.init("bus_list");
+
+        //Restart
+        document.getElementById("cancel_wait").addEventListener('click', function ()
+        {
+            controller.startGeoLocation(IS_DEBUG);
+        }, false);
+
+        if (IS_QUIK)
+        {
+            controller = new SurrealRanch_Controller.Controller(lat_in, lng_in, city_code, mode, zoom_level, wait_widow);
+            controller.document_width = $(document).width();
+            controller.document_height = $(document).height();
+            controller.quickInit('map_canvas', 'wait_window');
+
+            var url = "http://geopad.ca/js/get_json_for.php?trips=3017b77_560b94_468b95_112";
+            statusPage.makeList(url);
+            google.maps.event.addListener(statusPage, 'make_status_done', function (status) {
+                controller.hideWaitWindow();
+                if (status == "SUCCESS") {
+                    //alert(status);
+                    controller.hideMap();
+                    statusPage.showPage();
+                }
+                else {
+                    alert(status);
+                }
+            });
+        }
+        else
+        {
+            controller = new SurrealRanch_Controller.Controller(lat_in, lng_in, city_code, mode, zoom_level, wait_widow);
+            controller.startGeoLocation(IS_DEBUG);
+            controller.document_width = $(document).width();
+            controller.document_height = $(document).height();
+
+            google.maps.event.addListener(controller.peg_marker, 'geolocation_error', function (errMsg) {
+                document.getElementById("wait_msg").innerHTML = errMsg;
+                document.getElementById("refresh_btn").className = "show";
+            });
+
+            google.maps.event.addListener(controller.peg_marker, 'geolocation_done', function(result)
+            {
+                google_map = controller.initialize(result, 'map_canvas', 'wait_window');
+
+                google.maps.event.addListener(controller, 'draw_routes_error', function(errMsg)
+                {
+                    controller.hideWaitWindow();
+                    alert(errMsg);
+                });
+
+                google.maps.event.addListener(statusPage, 'make_status_done', function (status) {
+                    controller.hideWaitWindow();
+                    if (status == "SUCCESS") {
+                        controller.hideMap();
+                        statusPage.showPage();
+                        document.getElementById("bus_list").className = "hide";
+                    }
+                    else {
+                        alert(status);
+                    }
+                });
+            });
+        }
+    });
+});
+
---
+++ www/js/startup.js
@@ -0,0 +1,52 @@
+﻿var startUpPage = {
+    start_up_page: null,
+    map_canvas: null,
+    bus_list: null,
+    status_page: null,
+    _networkState: null,
+    init: function (start_up_page_id, map_canvas_id, bus_list_id, status_page_id)
+    {
+        _states = {};
+        _states[Connection.UNKNOWN] = 'Unknown connection';
+        _states[Connection.ETHERNET] = 'Ethernet connection';
+        _states[Connection.WIFI] = 'WiFi connection';
+        _states[Connection.CELL_2G] = 'Cell 2G connection';
+        _states[Connection.CELL_3G] = 'Cell 3G connection';
+        _states[Connection.CELL_4G] = 'Cell 4G connection';
+        _states[Connection.CELL] = 'Cell generic connection';
+        _states[Connection.NONE] = 'No network connection';
+
+        this.start_up_page = document.getElementById(start_up_page_id);
+        this.start_up_page.className = "hide";
+        this.map_canvas = document.getElementById(map_canvas_id);
+        this.bus_list = document.getElementById(bus_list_id);
+        this.status_page = document.getElementById(status_page_id);
+        document.addEventListener("offline", this.goinOffLine.bind(this), false);
+        document.addEventListener("online", this.commingOnLine.bind(this), false);
+    },
+    goinOffLine: function ()
+    {
+        //alert("Going Off Line");
+        //close any thing that's open
+        this.map_canvas.className = "hide";
+        this.bus_list.className = "hide";
+        this.status_page.className = "hide";
+
+        this.start_up_page.className = "show";
+    },
+    commingOnLine: function ()
+    {
+        //alert("Coming On Line");
+        this.start_up_page.className = "hide";
+        this.map_canvas.className = "show";
+    },
+    checkConnection: function()
+    {
+        _networkState = navigator.connection.type;
+        //alert('Connection type: ' + _states[_networkState]);
+        if (_networkState == Connection.UNKNOW || _networkState == Connection.NONE)
+        {
+            this.goinOffLine();
+        }
+    }
+};
